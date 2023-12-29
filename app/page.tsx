f97f2a7b9284530721539d0f35d913a109c1052e
YashwanthGudala <yashwanthgudala7@gmail.com>
2023-12-29 14:07:27 +0530

    added org

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,217 @@
-import React from 'react'
-import { Tree, TreeNode } from 'react-organizational-chart';
-import './style.module.css' ;
-const Home = () => {
-  return  (
-    <>         
-    <div className="container">
-  <h1 className="level-1 rectangle">CEO</h1>
-  <ol className="level-2-wrapper">
-    <li>
-      <h2 className="level-2 rectangle">Director A</h2>
-      <ol className="level-3-wrapper">
-        <li>
-          <h3 className="level-3 rectangle">Manager A</h3>
-          <ol className="level-4-wrapper">
-            <li>
-              <h4 className="level-4 rectangle">Person A</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person B</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person C</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person D</h4>
-            </li>
-          </ol>
-        </li>
-        <li>
-          <h3 className="level-3 rectangle">Manager B</h3>
-          <ol className="level-4-wrapper">
-            <li>
-              <h4 className="level-4 rectangle">Person A</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person B</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person C</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person D</h4>
-            </li>
-          </ol>
-        </li>
-      </ol>
-    </li>
-    <li>
-      <h2 className="level-2 rectangle">Director B</h2>
-      <ol className="level-3-wrapper">
-        <li>
-          <h3 className="level-3 rectangle">Manager C</h3>
-          <ol className="level-4-wrapper">
-            <li>
-              <h4 className="level-4 rectangle">Person A</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person B</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person C</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person D</h4>
-            </li>
-          </ol>
-        </li>
-        <li>
-          <h3 className="level-3 rectangle">Manager D</h3>
-          <ol className="level-4-wrapper">
-            <li>
-              <h4 className="level-4 rectangle">Person A</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person B</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person C</h4>
-            </li>
-            <li>
-              <h4 className="level-4 rectangle">Person D</h4>
-            </li>
-          </ol>
-        </li>
-      </ol>
-    </li>
-  </ol>
-</div>
 
-<footer className="page-footer">
-  <span>made by </span>
-  <a href="https://georgemartsoukos.com/" target="_blank">
-    <img width="24" height="24" src="https://assets.codepen.io/162656/george-martsoukos-small-logo.svg" alt="George Martsoukos logo" />
-  </a>
-</footer>
-    </>
+ // Import necessary dependencies
+import React from 'react';
+import styles from './styles.module.css';
+
+const treeData : any[] = [
+  {
+      "id": 11,
+      "status": "ACTIVE",
+      "handledEmployeeId": 8,
+      "handledEmployeeFullName": "Gudala Yashwanth",
+      "clientName": "Google Pay",
+      "projects": [
+          {
+              "id": 22,
+              "projectName": "gpay 2.0",
+              "clientId": 11,
+              "clientName": "Google Pay",
+              "projectStatus": "IN_PROGRESS",
+              "projectManagerId": 9,
+              "projectManagerFullName": "Yadhaiah Oggu",
+              "employeeProjectAssignmentNodes": [
+                  {
+                      "id": 13,
+                      "projectAssignmentStatus": "ACTIVE",
+                      "designation": "Manager",
+                      "employeeId": 9,
+                      "supervisorId": 8,
+                      "employeeFullName": "Yadhaiah Oggu",
+                      "supervisorFullName": "Gudala Yashwanth",
+                      "supervisedAssignments": [
+                          {
+                              "id": 14,
+                              "projectAssignmentStatus": "ACTIVE",
+                              "designation": "Tech lead",
+                              "employeeId": 11,
+                              "supervisorId": 9,
+                              "employeeFullName": "Pranay Chilumala",
+                              "supervisorFullName": "Yadhaiah Oggu",
+                              "supervisedAssignments": [
+                                  {
+                                      "id": 16,
+                                      "projectAssignmentStatus": "ACTIVE",
+                                      "designation": "TESTER 1",
+                                      "employeeId": 12,
+                                      "supervisorId": 11,
+                                      "employeeFullName": "dummy1 dummy1",
+                                      "supervisorFullName": "Pranay Chilumala",
+                                      "supervisedAssignments": [],
+                                      "projectId": 22,
+                                      "projectName": "gpay 2.0"
+                                  },
+                                  {
+                                      "id": 17,
+                                      "projectAssignmentStatus": "ACTIVE",
+                                      "designation": "TESTER 2",
+                                      "employeeId": 13,
+                                      "supervisorId": 11,
+                                      "employeeFullName": "dummy2 dummy",
+                                      "supervisorFullName": "Pranay Chilumala",
+                                      "supervisedAssignments": [],
+                                      "projectId": 22,
+                                      "projectName": "gpay 2.0"
+                                  }
+                              ],
+                              "projectId": 22,
+                              "projectName": "gpay 2.0"
+                          },
+                          {
+                              "id": 15,
+                              "projectAssignmentStatus": "ACTIVE",
+                              "designation": "UI",
+                              "employeeId": 10,
+                              "supervisorId": 9,
+                              "employeeFullName": "Vijay Varala",
+                              "supervisorFullName": "Yadhaiah Oggu",
+                              "supervisedAssignments": [],
+                              "projectId": 22,
+                              "projectName": "gpay 2.0"
+                          }
+                      ],
+                      "projectId": 22,
+                      "projectName": "gpay 2.0"
+                  }
+              ]
+          },
+          {
+              "id": 23,
+              "projectName": "GPAY BILL",
+              "clientId": 11,
+              "clientName": "Google Pay",
+              "projectStatus": "IN_PROGRESS",
+              "projectManagerId": 10,
+              "projectManagerFullName": "Vijay Varala",
+              "employeeProjectAssignmentNodes": [
+                  {
+                      "id": 18,
+                      "projectAssignmentStatus": "ACTIVE",
+                      "designation": "MANAGER",
+                      "employeeId": 10,
+                      "supervisorId": 8,
+                      "employeeFullName": "Vijay Varala",
+                      "supervisorFullName": "Gudala Yashwanth",
+                      "supervisedAssignments": [
+                          {
+                              "id": 19,
+                              "projectAssignmentStatus": "ACTIVE",
+                              "designation": "JAVA DEV",
+                              "employeeId": 9,
+                              "supervisorId": 10,
+                              "employeeFullName": "Yadhaiah Oggu",
+                              "supervisorFullName": "Vijay Varala",
+                              "supervisedAssignments": [
+                                  {
+                                      "id": 20,
+                                      "projectAssignmentStatus": "ACTIVE",
+                                      "designation": "JAVA DEV",
+                                      "employeeId": 12,
+                                      "supervisorId": 9,
+                                      "employeeFullName": "dummy1 dummy1",
+                                      "supervisorFullName": "Yadhaiah Oggu",
+                                      "supervisedAssignments": [
+                                          {
+                                              "id": 21,
+                                              "projectAssignmentStatus": "ACTIVE",
+                                              "designation": "JAVA DEV",
+                                              "employeeId": 13,
+                                              "supervisorId": 12,
+                                              "employeeFullName": "dummy2 dummy",
+                                              "supervisorFullName": "dummy1 dummy1",
+                                              "supervisedAssignments": [
+                                                  {
+                                                      "id": 22,
+                                                      "projectAssignmentStatus": "ACTIVE",
+                                                      "designation": "QA JUNIOR",
+                                                      "employeeId": 14,
+                                                      "supervisorId": 13,
+                                                      "employeeFullName": "dummy4 dummy",
+                                                      "supervisorFullName": "dummy2 dummy",
+                                                      "supervisedAssignments": [],
+                                                      "projectId": 23,
+                                                      "projectName": "GPAY BILL"
+                                                  }
+                                              ],
+                                              "projectId": 23,
+                                              "projectName": "GPAY BILL"
+                                          }
+                                      ],
+                                      "projectId": 23,
+                                      "projectName": "GPAY BILL"
+                                  }
+                              ],
+                              "projectId": 23,
+                              "projectName": "GPAY BILL"
+                          }
+                      ],
+                      "projectId": 23,
+                      "projectName": "GPAY BILL"
+                  }
+              ]
+          }
+      ]
+  }
+];
+
+const App = () => {
+  return (
+    <div className={styles.tree}>
+      {treeRendering(treeData)}
+    </div>
   );
-}
+};
 
-export default Home
+const treeRendering = (treeData) => {
+  return (
+    <ul>
+      {treeData.map((client) => (
+        <li key={client.id}>
+          <div>{client.clientName} - Handled By: {client.handledEmployeeFullName}</div>
+          {client.projects && client.projects.length > 0 && renderProjects(client.projects)}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const renderProjects = (projects) => {
+  return (
+    <ul>
+      {projects.map((project) => (
+        <li key={project.id}>
+          <div>{project.projectName} - Project Manager: {project.projectManagerFullName}</div>
+          {project.employeeProjectAssignmentNodes && project.employeeProjectAssignmentNodes.length > 0 && (
+            <ul>
+              {renderAssignments(project.employeeProjectAssignmentNodes)}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const renderAssignments = (assignments) => {
+  return assignments.map((assignment) => (
+    <li key={assignment.id}>
+      <div>{assignment.designation} - Assigned To: {assignment.employeeFullName}</div>
+      {assignment.supervisedAssignments && assignment.supervisedAssignments.length > 0 && (
+        <ul>
+          {renderAssignments(assignment.supervisedAssignments)}
+        </ul>
+      )}
+    </li>
+  ));
+};
+
+export default App;
